ee1b94aec13a9038ffe3b882f9b88e39fb2bd356
Julian Palco <palcojulian@outlook.com>
2023-08-14 23:42:47 -0500

    Proyecto para aprender a usar storage de pinia

---
+++ src/counter/pages/CounterOnePage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useCounterOptionsStore } from '@/store/counter-options';
+
+
+const counterOptions = useCounterOptionsStore();
+
+
+</script>
+
+<template>
+
+    <h4>Counter: {{ counterOptions.count }}</h4>
+    <h4>CounterSquared: {{ counterOptions.squareCount }} </h4>
+</template>
+
+
+<style scoped>
+
+</style>
